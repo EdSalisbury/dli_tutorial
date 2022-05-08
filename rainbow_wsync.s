00c387f06ac02a2d6f2130c3f00bbd4ec97399c5
Ed Salisbury <ed.salisbury@gmail.com>
2022-05-07 19:12:08 -0600

    Added rainbow_wsync

---
+++ rainbow_wsync.s
@@ -0,0 +1,48 @@
+; Written in 2019 by Rob McMullen, https://playermissile.com/dli_tutorial/
+; Copyright and related rights waived via CC0: https://creativecommons.org/publicdomain/zero/1.0/
+        *= $3000
+; Updated for assembly with MADS by Ed Salisbury 2022/05/07
+
+    icl "hardware.s"
+
+    org $3000
+
+temp_color = $80
+
+init
+    ; load display list & fill with test data
+    jsr init_dli_screen_mode4
+
+    ; load display list interrupt address
+    ldx #>dli
+    ldy #<dli
+    jsr init_dli
+
+    jmp forever
+
+    icl "util.s"
+    icl "util_dli.s"
+
+dli pha             ; save A & X registers to stack
+    txa
+    pha
+    ldx #16         ; Make 16 color changes
+    lda #$a         ; initial color
+    sta WSYNC       ; first WSYNC gets us to the start of scan line we want
+?loop sta COLBK     ; change background color
+    clc
+    adc #$10        ; change color value, luminance remains the same
+    dex             ; update iteration count
+    sta WSYNC       ; make it the color change last ...
+    sta WSYNC       ;   for two scan lines
+    bne ?loop       ; sta doesn't affect processor flags so we are still checking result of dex
+    lda #$00        ; reset background color to black
+    sta COLBK
+    pla             ; restore X & A registers from stack
+    tax
+    pla
+    rti             ; always end DLI with RTI!
+
+; tell DOS where to run the program when loaded
+    org $2e0
+    .word init
